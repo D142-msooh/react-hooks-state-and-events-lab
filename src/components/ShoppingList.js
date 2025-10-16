57ef1e7b1b5d3b82408d94020d12ef3720c550ea
D142-msooh <kinyajuidama176@gmail.com>
2025-10-16 08:27:54 +0300

    Add files

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
+import itemData from "../data/items";
 
-function ShoppingList({ items }) {
+function ShoppingList() {
+  const [items, setItems] = useState(itemData);
+
+  function handleFilterChange(event) {
+    const selectedCategory = event.target.value;
+    if (selectedCategory === "All") {
+      setItems(itemData);
+    } else {
+      const filteredItems = itemData.filter(
+        (item) => item.category === selectedCategory
+      );
+      setItems(filteredItems);
+    }
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
